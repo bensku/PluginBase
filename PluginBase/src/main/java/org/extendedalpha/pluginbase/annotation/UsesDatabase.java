af15214f2764f783f57657017a004e8c61c5566e
Exiled <exiledev@hotmail.com>
2015-10-06 16:24:52 -0700

    Added annotations for plugins to use if you are to lazy to make / edit plugin.yml

---
+++ PluginBase/src/main/java/org/extendedalpha/pluginbase/annotation/UsesDatabase.java
@@ -0,0 +1,13 @@
+package org.extendedalpha.pluginbase.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ *  Part of the plugin annotations framework.
+ *  <p>
+ *  Denotes this plugin as using Bukkit's bundled database system.
+ */
+
+@Target(ElementType.TYPE)
+public @interface UsesDatabase {}
